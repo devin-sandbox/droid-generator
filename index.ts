6fe2779a0109d6f846ebd0bf0ef6de61ee150d73
jigsaw <m@jgs.me>
2025-02-16 13:04:50 +0900

    parse/stringify ini

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import { IniMap } from "./ini";
+
+const ini = new IniMap();
+
+const raw = await Bun.file("./patches/1.ini").text();
+const config = ini.parse(raw);
+await Bun.write("./gen/1.1.ini", config.toString());
